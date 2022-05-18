aac6b25b71808b1d96db662c511fe9454dc84bc6
Lisa DeSpain <lisamdespain@gmail.com>
2022-05-18 13:52:22 -0500

    initial commit all wired up

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const initialState = { count: 0 }
+    function reducer(state = initialState, action) {
+      return state
+    }
+let store
+    export const resetStore = () => {
+      const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+      store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+    }
+    resetStore()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+        <App />
+      </Provider>,
   document.getElementById('root')
 );
